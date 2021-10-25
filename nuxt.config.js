63d9bbf63bec80cab81542a0dea26ac8750608a1
tqthien <tqthien@ascvn.edu.vn>
2021-10-25 13:42:38 +0700

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   ssr: true,
+  router: {
+    base: '/nuxt-dks/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
